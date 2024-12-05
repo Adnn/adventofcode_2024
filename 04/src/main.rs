4cbd36c15ddd3770f6147aeff8af1fc416026653
Adrien <Adnn@users.noreply.github.com>
2024-12-05 15:33:38 +0100

    Solve d04_p2.

--- 04/src/main.rs
+++ 04/src/main.rs
@@ -44,37 +44,68 @@ impl WordSearch {
         }
     }
 
-    fn search(&self, idx: usize, pattern: &str) -> u32 {
+    fn search_8directions(&self, idx: usize, pattern: &str) -> u32 {
+        // compute how much room is left on the rectangular input from idx.
+        // left, right, top, bottom.
         let x0 = idx % self.width;
         let x1 = self.width - x0 - 1;
         let y0 = idx / self.height;
         let y1 = self.height - y0 - 1;
         //println!("for idx '{idx}', ({x0}, {x1}), ({y0}, {y1})");
 
+        let min_room = pattern.len() - 1;
         let mut count = 0;
-        if x0 >= pattern.len() && match_pattern(&self.input, pattern, idx, -1) {
+        if x0 >= min_room && match_pattern(&self.input, pattern, idx, -1) {
             count += 1;
         }
-        if x1 >= pattern.len() && match_pattern(&self.input, pattern, idx, 1) {
+        if x1 >= min_room && match_pattern(&self.input, pattern, idx, 1) {
             count += 1;
         }
-        if y0 >= pattern.len() && match_pattern(&self.input, pattern, idx, -(self.width as i32)) {
+        if y0 >= min_room && match_pattern(&self.input, pattern, idx, -(self.width as i32)) {
             count += 1;
         }
-        if y1 >= pattern.len() && match_pattern(&self.input, pattern, idx, self.width as i32) {
+        if y1 >= min_room && match_pattern(&self.input, pattern, idx, self.width as i32) {
             count += 1;
         }
-        if x0 >= pattern.len() && y0 >= pattern.len() && match_pattern(&self.input, pattern, idx, -1-(self.width as i32)) {
+        if x0 >= min_room && y0 >= min_room && match_pattern(&self.input, pattern, idx, -1-(self.width as i32)) {
             count += 1;
         }
-        if x0 >= pattern.len() && y1 >= pattern.len() && match_pattern(&self.input, pattern, idx, -1+(self.width as i32)) {
+        if x0 >= min_room && y1 >= min_room && match_pattern(&self.input, pattern, idx, -1+(self.width as i32)) {
             count += 1;
         }
-        if x1 >= pattern.len() && y0 >= pattern.len() && match_pattern(&self.input, pattern, idx, 1-(self.width as i32)) {
+        if x1 >= min_room && y0 >= min_room && match_pattern(&self.input, pattern, idx, 1-(self.width as i32)) {
             count += 1;
         }
-        if x1 >= pattern.len() && y1 >= pattern.len() && match_pattern(&self.input, pattern, idx, 1+(self.width as i32)) {
+        if x1 >= min_room && y1 >= min_room && match_pattern(&self.input, pattern, idx, 1+(self.width as i32)) {
             count += 1;
+        }
+        count
+    }
+
+    fn search_diagonals(&self, idx: usize, pattern: &str) -> u32 {
+        // compute how much room is left on the rectangular input from idx.
+        // left, right, top, bottom.
+        let x0 = idx % self.width;
+        let x1 = self.width - x0 - 1;
+        let y0 = idx / self.height;
+        let y1 = self.height - y0 - 1;
+
+        let mut count = 0;
+        // If there is at least 1 position on each direction (to fit the X)
+        if x0 >= 1 && x1 >= 1 && y0 >= 1 && y1 >= 1 {
+            let diag_1_inc = 1 + self.width as i32;
+            let top_left = idx as i32 - diag_1_inc as i32;
+            let bottom_right = idx as i32 + diag_1_inc as i32;
+            let diag_2_inc = 1 - self.width as i32;
+            let bottom_left = idx as i32 - diag_2_inc as i32;
+            let top_right = idx as i32 + diag_2_inc as i32;
+            if (match_pattern(&self.input, pattern, top_left as usize, diag_1_inc)
+                || match_pattern(&self.input, pattern, bottom_right as usize, -diag_1_inc))
+                &&
+                (match_pattern(&self.input, pattern, bottom_left as usize, diag_2_inc)
+                || match_pattern(&self.input, pattern, top_right as usize, -diag_2_inc)) {
+                count += 1;
+            }
         }
         count
     }
@@ -83,8 +114,8 @@ impl WordSearch {
 fn match_pattern(text: &Vec<char>, pattern: &str, start: usize, inc: i32) -> bool {
     for (i, c) in pattern.chars().enumerate() {
         //let position: i32 = start as i32 + (inc as i32 * i);
-        let position: i32 = start as i32 + (i as i32 + 1) * inc;
-        if ! (text[position as usize] == c) {
+        let position: i32 = start as i32 + (i as i32) * inc;
+        if text[position as usize] != c {
             return false;
         }
     }
@@ -97,14 +128,22 @@ fn main() {
     //println!("Content: {content}");
 
 
-    let mut total = 0;
+    let mut total_1 = 0;
+    let mut total_2 = 0;
     let puzzle = WordSearch::make(&mut content);
     for (idx, c) in puzzle.input.iter().enumerate() {
         if *c == 'X' {
-            total += puzzle.search(idx, "MAS");
+            // Wastefully test for X a second time.
+            total_1 += puzzle.search_8directions(idx, "XMAS");
+        }
+        else if *c == 'A' {
+            total_2 += puzzle.search_diagonals(idx, "MAS");
         }
     }
-    println!("Total occurrences of 'XMAS' in the puzzle ({}, {}): {total}",
+    println!(
+    r#"In the puzzle ({}, {})
+    - occurrences of "XMAS": {total_1}
+    - occurrences of "MAS" in a X: {total_2}"#,
         puzzle.width,
         puzzle.height);
 }
